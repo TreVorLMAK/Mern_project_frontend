ca497269466cae73df9b0247131d0a858f026e60
TreVorLMAK <bimalbhandari563@gmail.com>
2024-12-02 21:46:58 +0545

    get api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Card from '../components/Card'
 
 const Home = () => {
+        const fetchBlogs = async ()=>{
+        const response =  await axios.get("https://localhost:3000/blog")
+        }
+
+    useEffect(()=>{
+        fetchBlogs()
+
+    },[])
   return (
     <>
     <Navbar/>
