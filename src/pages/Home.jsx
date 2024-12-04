d2172eb828b90966f7967bb76a37d5d7b2ddb40f
TreVorLMAK <bimalbhandari563@gmail.com>
2024-12-04 21:15:04 +0545

    home page integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Card from '../components/Card'
+import axios from 'axios'
 
 const Home = () => {
+        const [blogs, setBlogs] = useState([])
         const fetchBlogs = async ()=>{
-        const response =  await axios.get("https://localhost:3000/blog")
-        }
+        const response =  await axios.get("http://localhost:3000/blog")
+        setBlogs(response.data.data)
+       }
 
     useEffect(()=>{
         fetchBlogs()
-
+      // console.log(blogs)
     },[])
   return (
     <>
     <Navbar/>
     <div className='flex flex-wrap'>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
+      {
+      blogs.map(function(blog){
+        return(
+          <Card blog= {blog}/>
+        )
+      })
+    }
+
     </div>
     </>
   )
