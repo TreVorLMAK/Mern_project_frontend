f69313cd9bfb842d95fdb53897bf6cef24f7b444
TreVorLMAK <bimalbhandari563@gmail.com>
2024-12-09 21:02:53 +0545

    create blog ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import SingleBlog from './pages/SingleBlog'
+import CreateBlog from './pages/CreateBlog'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/blog/:id" element = {<SingleBlog />}/>
+      <Route path='/create' element = {<CreateBlog/>}/>
     </Routes>
     </BrowserRouter>
   )
