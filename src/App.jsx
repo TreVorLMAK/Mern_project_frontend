e796a18c899308f2b56874f45f913d27c2469c07
TreVorLMAK <bimalbhandari563@gmail.com>
2024-12-10 21:15:51 +0545

    edit api integration

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import SingleBlog from './pages/SingleBlog'
 import CreateBlog from './pages/CreateBlog'
+import EditBlog from './pages/EditBlog'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/blog/:id" element = {<SingleBlog />}/>
       <Route path='/create' element = {<CreateBlog/>}/>
+      <Route path='/edit/:id' element = {<EditBlog/>}/>
     </Routes>
     </BrowserRouter>
   )
