5ecfd82a3e9f31d4d5cb431ccd35087585717101
TreVorLMAK <bimalbhandari563@gmail.com>
2024-12-11 22:26:29 +0545

    final changes

--- src/pages/EditBlog.jsx
+++ src/pages/EditBlog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import axios from 'axios'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -33,6 +33,22 @@ const EditBlog = () => {
             }
     }
 
+    const fetchSingleBlog = async()=>{
+      const response = await axios.get("http://localhost:3000/blog/" + id)
+      if(response.status === 200){
+        setData({
+          title: response.data.data.title,
+          subtitle: response.data.data.subtitle,
+          description: response.data.data.description
+        })
+      }else{
+        alert("Error")
+      }
+   }
+   useEffect(()=>{
+       fetchSingleBlog()
+   },[])
+
   return (
     <>
       <Navbar />
@@ -46,20 +62,20 @@ const EditBlog = () => {
               type="text"
               name="title"
               className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
-              placeholder="Title*" onChange={handleChange}
+              placeholder="Title*" onChange={handleChange} value={data.title}
             />
             <input
               type="text"
               name="subtitle"
               className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
-              placeholder="Subtitle *" onChange={handleChange}
+              placeholder="Subtitle *" onChange={handleChange} value={data.subtitle}
             />
           </div>
           <input
             type="file"
             name="image"
             className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
-            placeholder="Image*" onChange={handleChange}
+            placeholder="Image*" onChange={handleChange} 
           />
           <br />
           <textarea
@@ -67,8 +83,8 @@ const EditBlog = () => {
             id="text"
             cols="30"
             rows="10"
-            className="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-gray-300"
-            placeholder="Description*" onChange={handleChange}
+            className="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-black-300"
+            placeholder="Description*" onChange={handleChange} value={data.description}
           >
             
           </textarea>
